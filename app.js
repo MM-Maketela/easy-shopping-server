896e69bd29602eb3835ef335a333d9b01694136f
MM-Maketela <maketelamafa2@gmail.com>
2023-07-13 21:29:46 +0200

    backend server

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ app.use('/client/products',upload.any(),client_router)
 
 app.listen(process.env.SERVER_PORT, ()=>{
     console.log(`Server running at port ${process.env.SERVER_PORT}`)
-})
+})git 
